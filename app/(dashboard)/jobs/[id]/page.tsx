028303e07a95a17bd150cb524d9ecd85914334e9
ArminGreen <armingreen1@gmail.com>
2024-08-25 19:27:16 +0300

    fix /jobs/id

--- app/(dashboard)/jobs/[id]/page.tsx
+++ app/(dashboard)/jobs/[id]/page.tsx
@@ -1,21 +1,24 @@
 import EditJobForm from "@/components/EditJobForm";
 import { GetSingleJobAction } from "@/utils/actions";
+
 import {
   dehydrate,
   HydrationBoundary,
   QueryClient,
 } from "@tanstack/react-query";
 
-async function SingleJobPage({ params }: { params: { id: string } }) {
+async function JobDetailPage({ params }: { params: { id: string } }) {
   const queryClient = new QueryClient();
+
   await queryClient.prefetchQuery({
     queryKey: ["job", params.id],
     queryFn: () => GetSingleJobAction(params.id),
   });
+
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
       <EditJobForm jobId={params.id} />
     </HydrationBoundary>
   );
 }
-export default SingleJobPage;
+export default JobDetailPage;
