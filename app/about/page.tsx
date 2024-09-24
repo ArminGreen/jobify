49662438f4a989a57ed07a8220f8249c64462948
ArminGreen <armingreen1@gmail.com>
2024-09-24 18:34:59 +0300

    latest

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+function AboutPage() {
+  return (
+    <div>
+      <h1 className="text-right mr-12">كيف حالك</h1>
+    </div>
+  );
+}
+export default AboutPage;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,7 +10,7 @@ function Navbar() {
       </div>
       <div className="flex items-center gap-x-4">
         <ThemeToggle />
-        <UserButton afterSignOutUrl="/" />
+        <UserButton />
       </div>
     </nav>
   );
